8529a6b14b4088353cf70b5529ca2a12ede50441
Josh Mann <jlmann72@gmail.com>
2020-09-24 08:06:14 -0400

    CodeWar completed

--- Program.cs
+++ Program.cs
@@ -2,11 +2,28 @@
 
 namespace HumanReadableTime
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        public static string GetReadableTime(int seconds)
+        {
+
+            int hours = (seconds / 3600);
+            var minutesAndSeconds = seconds % 3600;
+            int minutes = minutesAndSeconds / 60;
+            int resultSeconds = minutesAndSeconds % 60;
+            string hourString = hours.ToString("00");
+            string minuteString = minutes.ToString("00");
+            string secondString = resultSeconds.ToString("00");
+            string result = $"{hourString}:{minuteString}:{secondString}";
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetReadableTime(5));
+
+
+
+        }
     }
-  }
 }
